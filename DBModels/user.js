5726b8dd68e630defaad8592d25a9a16857531d5
Kshitij Verma <kshituverma2004@gmail.com>
2026-03-10 20:15:16 +0530

    fix passport-local-mongoose plugin import

--- DBModels/user.js
+++ DBModels/user.js
@@ -1,15 +1,14 @@
-const { required } = require("joi");
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
-const passport_local_mongoose=require("passport-local-mongoose").default;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema=new Schema({
-     email:
-     {
-        type:String,
-        required:true
-    
-     }
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  }
 });
-userSchema.plugin(passport_local_mongoose);
-module.exports=mongoose.model('User',userSchema);
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
